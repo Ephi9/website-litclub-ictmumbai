fd5d9bbf4aa34226fa0ffc577a97a4faf538764d
Rohit Amol Patil <150115339+Ephi9@users.noreply.github.com>
2025-09-13 14:56:13 +0530

    Delete loaderanimation.js

--- loaderanimation.js
+++
@@ -1,8 +0,0 @@
-// --- Book Loader Animation ---
-const bookLoader = document.getElementById('book-loader');
-window.addEventListener('DOMContentLoaded', () => {
-    setTimeout(() => {
-        bookLoader.style.opacity = 0;
-        setTimeout(() => bookLoader.style.display = 'none', 700);
-    }, 1800); // matches book animation duration
-});
